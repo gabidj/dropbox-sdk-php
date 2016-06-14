41fea22b293d0a78e24776a2f7eafb6619f65d2b
Serghei Iakovlev <sergeyklay@users.noreply.github.com>
2016-06-14 12:59:45 +0300

    Clean up Path::checkArgNonRoot
    Closes #47

--- lib/Dropbox/Path.php
+++ lib/Dropbox/Path.php
@@ -163,9 +163,9 @@ final class Path
      */
     static function checkArgNonRoot($argName, $value)
     {
-        if ($value === null) throw new \InvalidArgumentException("'$argName' must not be null");
-        if (!is_string($value)) throw new \InvalidArgumentException("'$argName' must be a string");
+        Checker::argStringNonEmpty($argName, $value);
+
         $error = self::findErrorNonRoot($value);
-        if ($error !== null) throw new \InvalidArgumentException("'$argName'': bad path: $error: ".Util::q($value));
+        if ($error !== null) throw new \InvalidArgumentException("'$argName': bad path: $error: ".Util::q($value));
     }
 }
